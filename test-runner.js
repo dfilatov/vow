1addece28122592ea3107cf0a8eaada752898d88
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-15 01:48:03 +0400

    prevent run promises-aplus-tests if base tests failed

--- test-runner.js
+++ test-runner.js
@@ -1,3 +1,3 @@
-require('nodeunit').reporters.default.run(['test/promise.js'], null, function() {
-    require('promises-aplus-tests')(require('./test/aplus-adapter'));
+require('nodeunit').reporters.default.run(['test/promise.js'], null, function(error) {
+    error || require('promises-aplus-tests')(require('./test/aplus-adapter'));
 });
