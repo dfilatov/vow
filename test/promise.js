0fc14fb4f4e8d9228947741acb7dac68dbfb0a2a
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-15 21:42:52 +0400

    spread method added

--- test/promise.js
+++ test/promise.js
@@ -397,6 +397,20 @@ module.exports = {
         }
     },
 
+    'promise.spread' : {
+        'onFulfilled argument should be spreaded' : function(test) {
+            var promise = Promise();
+            promise.spread(function(arg1, arg2, arg3) {
+                test.strictEqual(arguments.length, 3);
+                test.strictEqual(arg1, 1);
+                test.strictEqual(arg2, '2');
+                test.strictEqual(arg3, true);
+                test.done();
+            });
+            promise.fulfill([1, '2', true]);
+        }
+    },
+
     'Promise.all' : {
         'resulting promise should be fulfilled after all promises fulfilled' : function(test) {
             var promises = [Promise(), Promise(), Promise()];
