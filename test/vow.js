59f4f56f3a4d0b37fd9ffc5a4f85dc91c22b8869
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-23 22:11:28 +0400

    Vow.resolve added

--- test/vow.js
+++ test/vow.js
@@ -505,6 +505,27 @@ module.exports = {
         }
     },
 
+    'Vow.resolve' : {
+        'resulting promise should be argument if it is promise' : function(test) {
+            var promise = Vow.promise(),
+                isFulfilled = promise.isFulfilled(),
+                resPromise = Vow.resolve(promise);
+
+            test.strictEqual(resPromise, promise);
+            test.strictEqual(resPromise.isFulfilled(), isFulfilled);
+            test.done();
+        },
+
+        'resulting promise should be fulfilled if argument is non-promise' : function(test) {
+            var promise = Vow.resolve('val');
+
+            promise.then(function(val) {
+                test.strictEqual(val, 'val');
+                test.done();
+            });
+        }
+    },
+
     'Vow.when' : {
         'onFullfilled callback should be called when argument fullfilled' : function(test) {
             var promise = Vow.promise();
