2ca81105e8e08f750978c83ca904a23780550b71
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-25 18:13:34 +0400

    Add static Vow.spread method (#9)

--- test/vow.js
+++ test/vow.js
@@ -490,6 +490,18 @@ module.exports = {
         }
     },
 
+    'Vow.spread' : {
+        'onFulfilled argument should be spreaded' : function(test) {
+            Vow.spread([1, '2', true], function(arg1, arg2, arg3) {
+                test.strictEqual(arguments.length, 3);
+                test.strictEqual(arg1, 1);
+                test.strictEqual(arg2, '2');
+                test.strictEqual(arg3, true);
+                test.done();
+            });
+        }
+    },
+
     'Vow.done' : {
         'exception should be throwed if argument is a promise' : function(test) {
             var promise = Vow.promise(),
