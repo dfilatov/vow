987c0da62e157a68ede170895c902afb5eda1507
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-12-26 11:00:02 +0400

    tests for promise.timeout() added

--- test/vow.js
+++ test/vow.js
@@ -446,6 +446,46 @@ module.exports = {
         }
     },
 
+    'promise.timeout' : {
+        'resulting promise should be rejected after timeout' : function(test) {
+            var resPromise = Vow.promise().timeout(10);
+            setTimeout(function() {
+                test.ok(resPromise.isRejected());
+                test.deepEqual(resPromise.valueOf(), new Error('timed out'));
+                test.done();
+            }, 20);
+        },
+
+        'resulting promise should be fulfilled before timeout' : function(test) {
+            var origPromise = Vow.promise(),
+                resPromise = origPromise.timeout(20);
+
+            setTimeout(function() {
+                origPromise.fulfill('val');
+            }, 10);
+
+            resPromise.then(function(val) {
+                test.strictEqual(val, 'val');
+                test.done();
+            });
+        },
+
+        'resulting promise should be rejected before timeout' : function(test) {
+            var origPromise = Vow.promise(),
+                resPromise = origPromise.timeout(20),
+                error = new Error('error');
+
+            setTimeout(function() {
+                origPromise.reject(error);
+            }, 10);
+
+            resPromise.then(null, function(_error) {
+                test.strictEqual(_error, error);
+                test.done();
+            });
+        }
+    },
+
     'Vow.when' : {
         'onFullfilled callback should be called when argument fullfilled' : function(test) {
             var promise = Vow.promise();
@@ -787,42 +827,12 @@ module.exports = {
     },
 
     'Vow.timeout' : {
-        'resulting promise should be rejected after timeout' : function(test) {
-            var resPromise = Vow.timeout(Vow.promise(), 10);
+        'resulting promise should not be rejected if arguments is not a promise' : function(test) {
+            var resPromise = Vow.timeout('a', 10);
             setTimeout(function() {
-                test.ok(resPromise.isRejected());
-                test.deepEqual(resPromise.valueOf(), new Error('timed out'));
+                test.ok(!resPromise.isRejected());
                 test.done();
             }, 20);
-        },
-
-        'resulting promise should be fulfilled before timeout' : function(test) {
-            var origPromise = Vow.promise(),
-                resPromise = Vow.timeout(origPromise, 20);
-
-            setTimeout(function() {
-                origPromise.fulfill('val');
-            }, 10);
-
-            resPromise.then(function(val) {
-                test.strictEqual(val, 'val');
-                test.done();
-            });
-        },
-
-        'resulting promise should be rejected before timeout' : function(test) {
-            var origPromise = Vow.promise(),
-                resPromise = Vow.timeout(origPromise, 20),
-                error = new Error('error');
-
-            setTimeout(function() {
-                origPromise.reject(error);
-            }, 10);
-
-            resPromise.then(null, function(_error) {
-                test.strictEqual(_error, error);
-                test.done();
-            });
         }
     }
 };
