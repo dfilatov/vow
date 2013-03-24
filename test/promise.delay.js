9b7207b6d74bf0b3db4a3ce46c082a544c17c73a
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-03-24 17:22:04 +0400

    delay methods behavior fixed

--- test/promise.delay.js
+++ test/promise.delay.js
@@ -1,39 +1,29 @@
 module.exports = {
-    'resulting promise should be fulfilled after timeout' : function(test) {
-        var resPromise = Vow.promise().delay(10, 'ok');
+    'resulting promise should be fulfilled after delay' : function(test) {
+        var origPromise = Vow.promise(),
+            resPromise = origPromise.delay(30);
+
+        setTimeout(function() {
+            test.ok(!resPromise.isResolved());
+        }, 20);
         setTimeout(function() {
             test.ok(resPromise.isFulfilled());
             test.strictEqual(resPromise.valueOf(), 'ok');
             test.done();
-        }, 20);
+        }, 40);
+        origPromise.fulfill('ok');
     },
 
-    'resulting promise should be fulfilled before timeout' : function(test) {
+    'resulting promise should be immediately rejected' : function(test) {
         var origPromise = Vow.promise(),
-            resPromise = origPromise.delay(20, 'ok');
+            resPromise = origPromise.delay(30);
 
         setTimeout(function() {
-            origPromise.fulfill('val');
+            test.ok(resPromise.isRejected());
+            test.strictEqual(resPromise.valueOf(), 'error');
+            test.done();
         }, 10);
 
-        resPromise.then(function(val) {
-            test.strictEqual(val, 'val');
-            test.done();
-        });
-    },
-
-    'resulting promise should be rejected before timeout' : function(test) {
-        var origPromise = Vow.promise(),
-            resPromise = origPromise.delay(20),
-            error = new Error('error');
-
-        setTimeout(function() {
-            origPromise.reject(error);
-        }, 10);
-
-        resPromise.then(null, function(_error) {
-            test.strictEqual(_error, error);
-            test.done();
-        });
+        origPromise.reject('error');
     }
 };
