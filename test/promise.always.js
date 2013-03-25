0558d5006c018345f8d2dc6a72a8fb42e6752746
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-03-25 16:54:12 +0400

    promise.always not pass onResolved returned value (#19)

--- test/promise.always.js
+++ test/promise.always.js
@@ -17,5 +17,31 @@ module.exports = {
             test.strictEqual(promise.valueOf(), 'error');
             test.done();
         });
+    },
+
+    'resulting promise should be fulfilled with returned value of onResolved callback' : function(test) {
+        var promise = Vow.promise();
+        promise.fulfill('ok');
+        promise
+            .always(function() {
+                return 'ok-always';
+            })
+            .then(function(val) {
+                test.strictEqual(val, 'ok-always');
+                test.done();
+            });
+    },
+
+    'resulting promise should be rejected with exception in onResolved callback' : function(test) {
+        var promise = Vow.promise();
+        promise.fulfill('ok');
+        promise
+            .always(function() {
+                throw 'error-always';
+            })
+            .fail(function(err) {
+                test.strictEqual(err, 'error-always');
+                test.done();
+            });
     }
 };
