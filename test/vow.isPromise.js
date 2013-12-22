03cef4e8fcd488aba660f710b669a5bcb5703a39
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-12-23 00:00:09 +0400

    internal refactoring

--- test/vow.isPromise.js
+++ test/vow.isPromise.js
@@ -4,8 +4,19 @@ module.exports = {
         test.done();
     },
 
+    'should be true if argument is thenable object' : function(test) {
+        test.ok(Vow.isPromise({ then : function() { } }));
+        test.done();
+    },
+
     'should be false if argument is non-promise' : function(test) {
+        test.ok(!Vow.isPromise(undefined));
         test.ok(!Vow.isPromise('val'));
+        test.ok(!Vow.isPromise(5));
+        test.ok(!Vow.isPromise(true));
+        test.ok(!Vow.isPromise({}));
+        test.ok(!Vow.isPromise(null));
+        test.ok(!Vow.isPromise(function() {}));
         test.done();
     }
 };
