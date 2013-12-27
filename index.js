43338b067694c836f9014dad2eccac27dd1b7036
dfilatov <dfilatov@yandex-team.ru>
2013-12-27 16:02:32 +0400

    add expand all / collapse all

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-$(document).on('click', '.details__opener', function(e) {
-    e.stopImmediatePropagation();
-    $(this)
-        .closest('.details')
-        .toggleClass('details_shown')
-        .find('.details__content:first')
-            .slideToggle();
-});
+$(document)
+    .on('click', '.details__opener', function(e) {
+        e.stopImmediatePropagation();
+        var details = $(this)
+                .closest('.details')
+                .toggleClass('details_shown'),
+            detailsContent = details.find('.details__content:first');
+
+        details.hasClass('details_shown')?
+            detailsContent.slideDown() :
+            detailsContent.slideUp();
+    })
+    .on('click', '.action', function() {
+        var expandAll = $(this).hasClass('action_type_expand-all'),
+            details = $('.details'),
+            detailsContents = $('.details__content');
+
+        details.toggleClass('details_shown', expandAll);
+        expandAll? detailsContents.show() : detailsContents.hide();
+    });
+
